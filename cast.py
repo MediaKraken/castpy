cdbf5e0418fc046d6b2e1846192c0deb3fc079fc
hugovk <hugovk@users.noreply.github.com>
2015-08-12 21:07:35 +0300

    Add --delay and --browsr arguments

--- cast.py
+++ cast.py
@@ -16,12 +16,25 @@ import string
 delay_millis = "10000"
 html = ''
 
+
+def open_in_browser(port):
+    import webbrowser
+    url = "http://localhost:" + str(port)
+    webbrowser.open(url, new=2)  # 2 = open in a new tab, if possible
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate a simple web '
                                      'slideshow for use with a Chromecast.')
     parser.add_argument(
         '-p', '--port', default=80, type=int,
         help='Port number.')
+    parser.add_argument(
+        '-d', '--delay', default=10, type=int,
+        help='Delay in seconds between images.')
+    parser.add_argument(
+        '-b', '--browser', action='store_true',
+        help="Automatically open the slideshow in a web browser.")
     args = parser.parse_args()
 
     images = os.listdir('img')
@@ -45,7 +58,7 @@ if __name__ == '__main__':
     # in milliseconds and generated list
     # of images.  Write the output to
     # index.html
-    payload = string.replace(payload, "$$1", delay_millis)
+    payload = string.replace(payload, "$$1", str(args.delay * 1000))
     payload = string.replace(payload, "$$2", html)
     with open("index.html", "w") as indexfile:
         indexfile.write(payload)
@@ -54,6 +67,8 @@ if __name__ == '__main__':
     Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
     httpd = SocketServer.TCPServer(("", args.port), Handler)
     print "HTTP server running..."
+    if args.browser:
+        open_in_browser(args.port)
     httpd.serve_forever()
 
 # End of file
